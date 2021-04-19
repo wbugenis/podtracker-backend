fb11af63354a8c82ca819ea26776187939895194
William Bugenis <wbugenis@gmail.com>
2021-04-19 04:28:13 -0400

    player refactors on front

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,16 +11,13 @@ class User < ApplicationRecord
     validates :password, presence: true, length: {minimum: 3} 
 
     def subscribe(podcast)
-        puts "hello"
-        puts podcast
         @podcast = Podcast.find_by(rss_feed: podcast[:rss_feed])
-        puts @podcast
+        
         if !@podcast
             puts "not found"
             @podcast = Podcast.create(title:podcast[:title], rss_feed:podcast[:rss_feed], podcast_img_url:podcast[:podcast_img_url], description:podcast[:description], podcast_home_url:podcast[:podcast_home_url])
         end
-        puts "after create"
-        puts @podcast
+
         @subscription = self.subscriptions.create(podcast_id: @podcast[:id])
         return @podcast
     end
