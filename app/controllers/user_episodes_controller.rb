187d43e3189f4dae328a2498848af128969767f4
William Bugenis <wbugenis@gmail.com>
2021-10-27 02:20:54 -0400

    removed debugging prints

--- app/controllers/user_episodes_controller.rb
+++ app/controllers/user_episodes_controller.rb
@@ -6,30 +6,20 @@ class UserEpisodesController < ApplicationController
     end
 
     def update
-        puts " ------"
-        puts "*****"
-        puts params
-        puts "****"
         @user_episode = UserEpisode.find(params[:id])
-        puts "***"
-        puts @user_episode.title
-        puts "***"
-        puts params[:listened]
-        puts "@@@@"
+
         if params[:current_time]
             @user_episode.update(current_time:params[:current_time])
         end
+
         if params[:listened].class == TrueClass || params[:listened].class == FalseClass
-            puts params[:listened].class
-            puts "************"
-            puts params[:listened]
             @user_episode.update(listened:params[:listened])
         end
+        
         render json: @user_episode
     end
 
     def retrieve
-        puts "retrieving"
         @user = User.find(params[:userid])
         @user_episodes = @user.user_episodes.where(podcast_id:params[:podcastid])
         render json: @user_episodes
